43d0d31016a9f410a6c3cd1036e141f8783c46d8
Phyllis Wong <wong.phyllism@gmail.com>
2018-10-16 11:25:28 -0700

    change app.css to App.css for uniformity

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react'
-
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import Navbar from '../components/Navbar/Navbar';
-import SideDrawer from '../components/SideDrawer/SideDrawer';
-import Backdrop from '../components/Backdrop/Backdrop';
+import Navbar from '../components/Navbar/Navbar'
+import SideDrawer from '../components/SideDrawer/SideDrawer'
+import Backdrop from '../components/Backdrop/Backdrop'
 
-import Landing from './landing/Landing';
-import Dashboard from './Dashboard/Dashboard';
+import Landing from './landing/Landing'
+import Dashboard from './Dashboard/Dashboard'
 
-import './App.css';
+import './App.css'
 
 
 class App extends Component {
   state = {
     sideDrawerOpen: false
-  };
+  }
 
   drawerToggleClickHandler = () => {
     this.setState( (prevState) => {
-      return { sideDrawerOpen: !prevState.sideDrawerOpen };
-    });
-  };
+      return { sideDrawerOpen: !prevState.sideDrawerOpen }
+    })
+  }
 
   backdropClickHandler = () => {
-    this.setState({ sideDrawerOpen: false });
-  };
+    this.setState({ sideDrawerOpen: false })
+  }
 
   render() {
     let backDrop;
 
     if (this.state.sideDrawerOpen) {
-      backDrop = <Backdrop click={this.backdropClickHandler} />;
+      backDrop = <Backdrop click={this.backdropClickHandler} />
     }
     return (
       <BrowserRouter>
@@ -49,4 +48,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
