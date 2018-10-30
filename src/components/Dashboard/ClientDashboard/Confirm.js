d78d8a5483377d5d5d713afd5e70adf65c3eca06
Phyllis Wong <wong.phyllism@gmail.com>
2018-10-30 12:20:08 -0700

    refactor css for confirm screen

--- src/components/Dashboard/ClientDashboard/Confirm.js
+++ src/components/Dashboard/ClientDashboard/Confirm.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
+import './Confirm.css'
 
 class Confirm extends Component {
   render() {
     return(
-      <div className={'taco'}>
-        <div className={"confirmation-container"}>
+      <div className={'confirmation-container'}>
+        <div className={"confirm-selected-time"}>
           <p className={"confirm-day"}>{ this.props.day.day }</p>
           <p className={"confirm-date"}>{ this.props.day.date }</p>
           <p className={"confirm-time"}>{ this.props.time }</p>
-          <button className={'confirm-select'}>select</button>
         </div>
-        <button onClick={ this.props.backButton }>change time</button>
+        <div className={'confirm-btns'}>
+          <button className={'confirm-btn btn-grey'} onClick={ this.props.backButton }>change</button>
+          <button className={'confirm-btn btn-blue'}>confirm</button>
+        </div>
       </div>
     )
   }
