a3e8b5354670e8eb452fcae180fb95c363406c81
Phyllis Wong <wong.phyllism@gmail.com>
2018-11-07 12:11:11 -0800

    fixed timepicker css

--- src/components/Dashboard/ClientDashboard/TimePicker.js
+++ src/components/Dashboard/ClientDashboard/TimePicker.js
@@ -12,10 +12,12 @@ class TimePicker extends Component {
 
     return (
       <div className={"time-picker-container"}>
-        <div className={"day-name"}>{this.props.activeDay.day}</div>
-        <div className={"day-date"}>{this.props.activeDay.date}</div>
+        {/* <div className={"day-name"}>{this.props.activeDay.day}</div> */}
+        <div className={"day-name"}>Monday</div>
+        {/* <div className={"day-date"}>{this.props.activeDay.date}</div> */}
+        <div className={"day-date"}>November 8, 2018</div>
         {times}
-        <button onClick={ this.props.backButton }>back</button>
+        <button className="back-btn-blue" onClick={ this.props.backButton }>back</button>
       </div>
     )
   }
