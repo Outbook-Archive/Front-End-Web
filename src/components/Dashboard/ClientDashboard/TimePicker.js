a304637f57db1a5bfe2aa2167feb8568f5115444
pomfy113 <pomfy113@gmail.com>
2018-11-09 13:52:41 -0800

    Minor comment and cleaning

--- src/components/Dashboard/ClientDashboard/TimePicker.js
+++ src/components/Dashboard/ClientDashboard/TimePicker.js
@@ -8,11 +8,11 @@ class TimePicker extends Component {
     // Array of Day mini COMPONENTS
     let timesSize = this.props.activeDay.times.length;
     let maxSize;
-    console.log(timesSize % 5)
 
+    // Math for items per row
     switch(timesSize % 5){
-      case 4:
       case 0:
+      case 4:
         maxSize = 5;
         break;
       case 3:
@@ -25,17 +25,21 @@ class TimePicker extends Component {
     }
 
     const rowAmount = timesSize / maxSize;
+
+    // Initializing rows
     let rows = new Array(Math.floor(rowAmount))
     for(let i = 0; i < rowAmount; i++){
       rows[i] = []
     }
 
+    // Set up rows
     this.props.activeDay.times.forEach((timeData, index) => {
       const timeObject = <Time time={timeData} clickedTime={() => this.props.clickedTime(timeData.time)}/>
       const row = Math.floor(index/maxSize)
       rows[row].push(timeObject)
     })
 
+    // Mapping time to rows
     const times = rows.map((row) => {
       return <div className="time-container-row">{row}</div>
     })
