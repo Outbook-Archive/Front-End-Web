b5424c97b325d64246f3dae1ff9ea67d4e5a7446
pomfy113 <pomfy113@gmail.com>
2018-11-09 12:48:26 -0800

    Checkpoint - flex movement

--- src/components/Dashboard/ClientDashboard/TimePicker.js
+++ src/components/Dashboard/ClientDashboard/TimePicker.js
@@ -6,17 +6,43 @@ class TimePicker extends Component {
 
   render() {
     // Array of Day mini COMPONENTS
+    let timesSize = this.props.activeDay.times.length;
+    let maxSize;
+
+    switch(timesSize % 5){
+      case 0:
+        maxSize = 5;
+        break;
+      case 4:
+        maxSize = 4;
+        break;
+      default:
+        maxSize = 3;
+        break;
+    }
+
+    const rowAmount = timesSize / maxSize;
+    let rows = new Array(Math.floor(rowAmount)).fill([])
+
     const times = this.props.activeDay.times.map((timeData, index) => {
       return <Time time={timeData} clickedTime={() => this.props.clickedTime(timeData.time)}/>
-    });
+    })
+
+    console.log(maxSize, timesSize)
+    times.forEach((item, index) => {
+      console.log(index / maxSize)
+      rows[Math.floor(index / maxSize)].push(item)
+    })
+
+    console.log(times)
+
+
 
     return (
       <div className="time-picker-container">
-        {/* <div className={"day-name"}>{this.props.activeDay.day}</div> */}
         <div className="day-name">{this.props.activeDay.day}</div>
-        {/* <div className={"day-date"}>{this.props.activeDay.date}</div> */}
         <div className="day-date">{this.props.activeDay.date}</div>
-        <div className="times-container">
+        <div className="time-container">
           {times}
         </div>
         <button className="back-btn-blue" onClick={ this.props.backButton }>back</button>
