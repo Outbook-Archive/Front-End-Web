fe90de61624ecab8eaa0c1b720955b88dfaca62d
Phyllis Wong <wong.phyllism@gmail.com>
2018-11-14 11:27:19 -0800

    refactor CSS with classnames

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,37 +4,37 @@ import './Login.css'
 class Login extends Component {
 	render() {
 		return (
-			<div className={'login-container'}>
-				<form>
-					<form-header>
+			<div className='login-container'>
+				<form className='form-container'>
+					<form-header className='form-header'>
 						<h1>Login in with Microsoft Account</h1>
 					</form-header>
 
 					<form-content>
-						<form>
+						<form className='form-content'>
 							{/* EMAIL */}
-							<form-group>
+							<form-group className='form-group'>
 								<label for="email">Email</label>
 								<input className='input-color' type="text" id="email" name="email" required />
 							</form-group>
 
 							{/* PASSWORD */}
-							<form-group>
+							<form-group className='form-group'>
 								<label for="password">Password</label>
 								<input className='input-color' type="text" id="password" name="password" required />
 							</form-group>
 
 							{/* CHECKBOX */}
-							<form-group>
+							<form-group className='form-group'>
 								<div className='remember-box'>
-                  <label className="form-remember">Remember Me</label>
-                  <input type="checkbox" />
+                  <input className='remember-checkbox' type="checkbox" />
+                  <label className="form-remember">Remember me</label>
 								</div>
-								<a href="/landing">Forgot password?</a>
+								<a className='forgot' href="/landing">Forgot password</a>
 							</form-group>
 
-							<form-group>
-								<button type="submit">Log In</button>
+							<form-group className='form-group'>
+								<button className='btn-submit' type="submit">Log In</button>
 							</form-group>
 
 						</form>
