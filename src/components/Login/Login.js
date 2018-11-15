a0b76216fc1f7a508bb487ff1778c7e5b90f142c
Phyllis Wong <wong.phyllism@gmail.com>
2018-11-15 11:12:44 -0800

    refactor component, simplify css, and point the button to the outlook signin

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react'
 import './Login.css'
 
 class Login extends Component {
+	componentWillMount(){
+    this.state = { auth: null };
+
+    fetch('http://outbook-us.herokuapp.com/').then(res =>{
+			return res.json();
+		}).then(data => {
+			this.setState({ auth: data.signInUrl });
+		});
+	}
+
 	render() {
 		return (
 			<div className='login-container'>
-				<form className='form-container'>
-					<form-header className='form-header'>
-						<h1 className='form-title'>Login with Microsoft account</h1>
-					</form-header>
+				<div className='form-container'>
 
-					<form-content>
-						<form className='form-content'>
-							<form-group className='form-group'>
-								<button className='btn-submit' type="submit">Sign in to Outlook</button>
-							</form-group>
-						</form>
-					</form-content>
-				</form>
+					<h1 className='form-title'>Login with Microsoft account</h1>
+					<button className='btn-submit' type="submit">
+						<a className='btn-link' href={this.state.auth} >Sign in to Outlook</a>
+					</button>
+
+				</div>
 			</div>
 		)
 	}
