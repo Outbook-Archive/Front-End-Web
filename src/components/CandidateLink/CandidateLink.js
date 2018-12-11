ca631927faa8bc50b7d164ee8064c77fd9dbcd06
Phyllis Wong <wong.phyllism@gmail.com>
2018-12-10 19:23:19 -0800

    first push to production

---
+++ src/components/CandidateLink/CandidateLink.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+import './CandidateLink.css';
+
+CandidateLink = () => {
+  return (
+    <div className='link-container'>
+      <input placeholder="984rkjnkjfnf98qhgkqerf"></input>
+      <button className='copy-btn'></button>
+    </div>
+  )
+}
+
+export default CandidateLink;
