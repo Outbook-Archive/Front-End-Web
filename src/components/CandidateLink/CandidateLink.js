574a92c8b17310cf5b6960d01d2c24427260c6d9
Phyllis Wong <wong.phyllism@gmail.com>
2018-12-10 23:35:57 -0800

    remove unused vars

--- src/components/CandidateLink/CandidateLink.js
+++ src/components/CandidateLink/CandidateLink.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './CandidateLink.css';
 
 function CandidateLink() {
+
+  const containerStyle = {
+    "marginTop": "10rem",
+    "background-color": "red"
+  }
+
   return (
     <div className='link-container'>
-      <input placeholder="984rkjnkjfnf98qhgkqerf"></input>
+      <input placeholder="984rkjnkjfnf98qhgkqerf" style={containerStyle} className='link-input'></input>
       <button className='copy-btn'></button>
     </div>
   )
