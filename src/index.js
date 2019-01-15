b576ba4e4d6268cdd535d7aa65bab49acd18f363
pomfy113 <pomfy113@gmail.com>
2019-01-15 15:06:59 -0800

    Hotfix, removed reducer dependency

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ src/index.js
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from './store';
+// import { Provider } from 'react-redux';
+// import configureStore from './store';
 
 import './index.css';
 import App from './components/App';
@@ -13,11 +13,11 @@ import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+  <App />,document.getElementById('root')
 );
+
+// <Provider store={configureStore()}>
+// </Provider>,
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
