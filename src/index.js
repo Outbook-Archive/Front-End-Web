81f22ae7b01e4f09e09dc5714ff882d51d1b2746
Phyllis Wong <wong.phyllism@gmail.com>
2019-01-25 14:44:08 -0800

    remove store.js, not using redux

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import configureStore from './store';
+// import configureStore from './store';
 
 import './index.css';
 import App from './components/App';
@@ -13,10 +13,7 @@ import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <App />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
