4a69bb476f48c3cd1511d7d1151c609ed8fb8bc3
Phyllis Wong <wong.phyllism@gmail.com>
2019-01-25 16:31:20 -0800

    add

--- src/components/CandidateLink/CandidateLink.js
+++ src/components/CandidateLink/CandidateLink.js
@@ -19,12 +19,20 @@ class CandidateLink extends Component {
       })
       .then( data => {
         console.log(data);
-        this.setState({ candidateLink: data.calendarUrl });
+        this.setState({ 
+          candidateLink: data.calendarUrl,
+          interviewerName: data.interviewerName
+         });
       })
   }
 
   copyTxt = () => {
-    // do something later
+    // write code to copy the text in the input field
+    console.log('Link: ', this.state.candidateLink)
+  }
+
+  copyToClipBoard = () => {
+    const el = 5;
   }
   
   
@@ -34,7 +42,7 @@ class CandidateLink extends Component {
         <h2>Welcome <span className='interviewer-name'>{ this.state.interviewerName }</span></h2>
         <p className='instructions'>Click the copy button to copy a link and paste it into an email to send to your interview candidate.</p>
         <div className='box-plus-btn'>
-          <div className='link-box' type='text'>
+          <div className='link-box' type='text' id='link-to-copy'>testing this value
             { this.state.candidateLink }
           </div>
           <button className='copy-btn' onClick={ this.copyTxt }>copy</button>
