b68a6a64b74d0cd8a01190015f5c5b69cfbb0791
Phyllis Wong <wong.phyllism@gmail.com>
2019-01-29 13:45:46 -0800

    remove unused vars

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-/*
-src/index.js
- */
+// src/index.js
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-// import configureStore from './store';
-
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
 
 
 ReactDOM.render(
